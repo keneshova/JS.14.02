8c176b1b441ba84e392ff7fd439d39a61734df31
Nestan <nestankeneshova16@gmail.com>
2023-02-14 14:33:58 +0600

    added a loop that counts from 1 to 10

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function sum(a, b, c){
   console.log(a + b + c);
 }
 sum(3, 4, 5);
+
+
+
+//обьем триугольника
+
+//считает от 1 до 10
+for (let v = 1; v <= 10; v++){
+  console.log(v); 
+}
+
+//массив из 10 чисел
+let example = [5, 7, 5, 2, 9, 3]
