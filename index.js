f1854479a9af4e6a850866d40eaf3526df0007e7
Nestan <nestankeneshova16@gmail.com>
2023-02-14 14:56:11 +0600

    added new them an object

--- index.js
+++ index.js
@@ -13,4 +13,28 @@ for (let v = 1; v <= 10; v++){
 }
 
 //массив из 10 чисел
-let example = [5, 7, 5, 2, 9, 3]
+let example = [5, 7, 5, 2, 9, 3];
+
+console.log(example);
+let result = 0;
+for (let i = 0; i < example.length; i++){
+  result = result + example[i];
+}
+console.log(result);
+
+
+//ОБЪЕКТЫ
+let example1 = {}
+let kesha = { name: "Kesha",
+ age: 22,
+ isEmployed: false
+ };//свойстве может бфть что угодно
+
+ let example2 = {}
+ let nestan = {
+  name:"Nestan",
+  age:17,
+  sureName: "Keneshova",
+  weight: 52,
+  height: 164,
+}
